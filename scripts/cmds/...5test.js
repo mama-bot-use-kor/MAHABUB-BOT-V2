a59a13762e666be20e6da44bd4d7aa3d16b25fe0
MR-OME-600 <ome153797@gmail.com>
2025-02-20 22:01:57 +0600

    Update ...5test.js

--- scripts/cmds/...5test.js
+++ scripts/cmds/...5test.js
@@ -1,46 +1,54 @@
-/** Custom Islamic Quotes Bot **/
-const fs = require("fs-extra");
+const axios = require("axios");
 const request = require("request");
-const asanic = require("asanic");
+const fs = require("fs-extra");
 
 module.exports = {
-  config: {
-    name: "islamic_quote",
-    version: "1.1.0",
-    permission: 0,
-    credits: "nayan (modified by [Your Name])",
-    description: "Sends a random Islamic quote with an image.",
-    prefix: true,
-    category: "user",
-    usages: "!islamic_quote",
-    cooldowns: 5,
-    dependencies: {}
-  },
+    config: {
+        name: "test",
+        version: "1.0",
+        author: "Converted from Mirai",
+        countDown: 5,
+        role: 0,
+        shortDescription: "Sends a random Islamic quote with an image",
+        longDescription: "This command will send a random Islamic quote along with a relevant image.",
+        category: "religion",
+        guide: "{prefix}islamic"
+    },
+    
+    onStart: async function ({ api, event }) {
+        const quotes = [
+            "ღ••\n– কোনো নেতার পিছনে নয়.!!🤸‍♂️\n– মসজিদের ইমামের পিছনে দাড়াও জীবন বদলে যাবে ইনশাআল্লাহ.!!🖤🌻",
+            "🌼 এত অহংকার করে লাভ নেই! 🌺 \nমৃত্যুটা নিশ্চিত,, শুধু সময়টা অ'নিশ্চিত।🖤🙂 ",
+            "_আল্লাহর ভালোবাসা পেতে চাও•••!🤗\n_তবে রাসুল (সা:)কে অনুসরণ করো••!🥰",
+            "🥀😒কেউ পছন্দ না করলে,,,,\n        কি যায় আসে,,🙂\n                😇আল্লাহ তো,,\n        পছন্দ করেই বানিয়েছে,,♥️🥀",
+            "_বুকে হাজারো কষ্ট নিয়ে\n                  আলহামদুলিল্লাহ বলাটা••!☺️\n_আল্লাহর প্রতি অগাধ বিশ্বাসের নমুনা❤️🥀"
+        ];
 
-  start: asanic(async function ({ api, event }) {
-    const quotes = [
-      "আল্লাহ সবকিছু দেখছেন, তাই ধৈর্য ধরো। 🌙",
-      "নামাজ কায়েম করো, সফলতা আসবেই! ☪️",
-      "সর্বদা সত্য বলো, কারণ ইসলাম শান্তির ধর্ম। 🤍",
-      "যারা ধৈর্য ধরে, তাদের জন্য আছে জান্নাতের সুসংবাদ। 🌷"
-    ];
+        const images = [
+            "https://i.postimg.cc/7LdGnyjQ/images-31.jpg",
+            "https://i.postimg.cc/65c81ZDZ/images-30.jpg",
+            "https://i.postimg.cc/Y0wvTzr6/images-29.jpg",
+            "https://i.postimg.cc/1Rpnw2BJ/images-28.jpg",
+            "https://i.postimg.cc/mgrPxDs5/images-27.jpg",
+            "https://i.postimg.cc/yxXDK3xw/images-26.jpg",
+            "https://i.postimg.cc/kXqVcsh9/muslim-boy-having-worship-praying-fasting-eid-islamic-culture-mosque-73899-1334.webp",
+            "https://i.postimg.cc/hGzhj5h8/muslims-reading-from-quran-53876-20958.webp",
+            "https://i.postimg.cc/x1Fc92jT/blue-mosque-istanbul-1157-8841.webp",
+            "https://i.postimg.cc/j5y56nHL/muhammad-ali-pasha-cairo-219717-5352.webp"
+        ];
 
-    const images = [
-      "https://example.com/my-islamic-image1.jpg",
-      "https://example.com/my-islamic-image2.jpg"
-    ];
+        const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+        const randomImage = images[Math.floor(Math.random() * images.length)];
 
-    const selectedQuote = quotes[Math.floor(Math.random() * quotes.length)];
-    const selectedImage = images[Math.floor(Math.random() * images.length)];
+        const imgPath = __dirname + "/cache/islamic.jpg";
 
-    const filePath = `${__dirname}/cache/islamic.jpg`;
-
-    request(selectedImage).pipe(fs.createWriteStream(filePath)).on("close", () => {
-      api.sendMessage(
-        { body: `「 ${selectedQuote} 」`, attachment: fs.createReadStream(filePath) },
-        event.threadID,
-        () => fs.unlinkSync(filePath)
-      );
-    });
-  })
+        request(randomImage)
+            .pipe(fs.createWriteStream(imgPath))
+            .on("close", () => {
+                api.sendMessage({
+                    body: `「 ${randomQuote} 」`,
+                    attachment: fs.createReadStream(imgPath)
+                }, event.threadID, () => fs.unlinkSync(imgPath));
+            });
+    }
 };
