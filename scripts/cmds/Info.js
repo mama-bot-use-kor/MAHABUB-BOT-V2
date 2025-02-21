835ea510fb7197d711e83ae8a255bb205ebe3005
MR-OME-600 <ome153797@gmail.com>
2025-02-21 08:44:35 +0600

    Update Info.js

--- scripts/cmds/Info.js
+++ scripts/cmds/Info.js
@@ -1,33 +1,36 @@
+const axios = require("axios");
+const fs = require("fs-extra");
+const path = require("path");
+const moment = require("moment-timezone");
+
 module.exports = {
-    name: "admin", // Command name (should match how you call it in the bot)
-    version: "1.0.0",
-    permission: 0,
-    credits: "nayan",
-    description: "Admin details and image",
-    prefix: true, // You can set this to true if you want it to be triggered with a prefix
-    cooldowns: 5,
-    dependencies: {
-        "axios": "",
-        "fs-extra": ""
+    config: {
+        name: "admin",
+        version: "1.0.0",
+        author: "nayan",
+        countDown: 5,
+        role: 0,
+        shortDescription: "Admin details and image",
+        longDescription: "Displays admin details along with an image",
+        category: "info",
+        guide: "{pn}"
     },
-    run: async function({ api, event, args, client, Users, Threads, __GLOBAL, Currencies }) {
-        const axios = require("axios");
-        const fs = require("fs-extra");
-        const moment = require("moment-timezone");
 
-        // Get bot uptime and format the time
-        const time = process.uptime();
-        const hours = Math.floor(time / (60 * 60));
-        const minutes = Math.floor((time % (60 * 60)) / 60);
-        const seconds = Math.floor(time % 60);
+    onStart: async function ({ api, event }) {
+        const cachePath = path.join(__dirname, "cache");
+        if (!fs.existsSync(cachePath)) fs.mkdirSync(cachePath);
 
-        // Set the timezone to Asia/Dhaka and format the current time
-        var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
+        const imagePath = path.join(cachePath, "admin.png");
+        const fbProfileUrl = `https://graph.facebook.com/100014754734049/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`;
 
-        // Send the response with personal information and the image
-        var callback = () => {
-            api.sendMessage({
-                body: `
+        try {
+            const response = await axios.get(fbProfileUrl, { responseType: "stream" });
+            const writer = fs.createWriteStream(imagePath);
+            response.data.pipe(writer);
+
+            writer.on("finish", async () => {
+                await api.sendMessage({
+                    body: `
 --------------------------------------------
 𝐍𝐚𝐦𝐞       : 𝐌𝐚𝐡𝐚𝐛𝐮𝐛 𝐑𝐚𝐡𝐦𝐚𝐧
 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝐈𝐬𝐥𝐚𝐦
@@ -42,18 +45,13 @@ module.exports = {
 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/.....
 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : ミ𝐌　𝐀　𝐇　𝐀　𝐁　𝐔　 𝐁ミ
 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/www.xnxx.com140`,
-                attachment: fs.createReadStream(__GLOBAL.cache + "/1.png")
-            }, event.threadID, () => fs.unlinkSync(__GLOBAL.cache + "/1.png"));
-        };
+                    attachment: fs.createReadStream(imagePath)
+                }, event.threadID, () => fs.unlinkSync(imagePath));
+            });
 
-        // Fetch the image from Facebook and save it
-        axios.get(encodeURI(`https://graph.facebook.com/100014754734049/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`), {
-            responseType: 'stream'
-        }).then(response => {
-            response.data.pipe(fs.createWriteStream(__GLOBAL.cache + '/1.png')).on('close', () => callback());
-        }).catch(error => {
+        } catch (error) {
             console.error("Error fetching image:", error);
-            api.sendMessage("Sorry, there was an error fetching the image.", event.threadID);
-        });
+            return api.sendMessage("Sorry, there was an error fetching the image.", event.threadID);
+        }
     }
 };
