23c6c812c9bc4583f0ea24d9de488589b5092454
MR᭄﹅ MAHABUB﹅ メꪜ <mrmahabub345@gmail.com>
2025-04-05 12:52:11 +0600

    Update autosend.js

--- scripts/cmds/autosend.js
+++ scripts/cmds/autosend.js
@@ -52,7 +52,7 @@ async function sendVideo(api, threadID, timeSlot) {
 function scheduleVideo(api) {
     const timeSlots = [
         "1:30 AM", "2:30 AM", "3:30 AM", "4:30 AM", "5:30 AM", "6:30 AM",
-        "7:30 AM", "8:30 AM", "9:30 AM", "10:30 AM", "11:30 AM", "12:49 PM",
+        "7:30 AM", "8:30 AM", "9:30 AM", "10:30 AM", "11:30 AM", "12:30 PM",
         "1:30 PM", "2:30 PM", "3:30 PM", "4:30 PM", "5:30 PM", "6:30 PM",
         "7:30 PM", "8:30 PM", "9:30 PM", "10:30 PM", "11:30 PM", "12:30 AM"
     ];
